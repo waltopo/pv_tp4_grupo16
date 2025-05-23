2acb4985ed9a5a3678e34a1979a619ca78e6f5df
MamaniErick <mamanierick64@gmail.com>
2025-05-23 02:11:57 -0300

    creado del formulario en ProductForm

--- tp4/src/components/ProductForm.jsx
+++ tp4/src/components/ProductForm.jsx
@@ -14,9 +14,14 @@ const ProductForm = ()=>{
     const [producto, setProducto] = useState(crearProducto);        
    
     return(
-        <>
-        
-        </>
+        <form>
+            <label>ID: <input name="id" type="number"/></label>
+            <label>Descripcion: <input name="descripcion" type="text"/></label>
+            <label>Precio: <input name="precioUnitario" type="number"/></label>
+            <label>Descuento %: <input name="descuento" type="number"/></label>
+            <label>Stock: <input name="stock" type="number"/></label>
+            <button></button>
+        </form>
     );
     
 }

6b789136384a15cb2708d848d602a2282bb7809b
MamaniErick <mamanierick64@gmail.com>
2025-05-23 01:32:04 -0300

    Creacion de codigo para cargar un producto

--- tp4/src/components/ProductForm.jsx
+++ tp4/src/components/ProductForm.jsx
@@ -9,18 +9,31 @@ const crearProducto ={
     stock: 0
 };
 
-const ProductForm = ()=>{
+const ProductForm = (editarProducto)=>{
 
     const [producto, setProducto] = useState(crearProducto);        
-   
+    
+    const handelSubmit = (e)=>{
+        e.preventDefault();
+        const precioConDescuento = producto.precioUnitario * (1-producto.descuento/100);
+        const product = { ...producto, precioConDescuento};
+        
+        if(editarProducto){
+            onAct(product);
+        }else{
+            onAgregar(product);
+        };
+        setProducto(crearProducto);
+    };
+
     return(
-        <form>
-            <label>ID: <input name="id" type="number"/></label>
-            <label>Descripcion: <input name="descripcion" type="text"/></label>
-            <label>Precio: <input name="precioUnitario" type="number"/></label>
-            <label>Descuento %: <input name="descuento" type="number"/></label>
-            <label>Stock: <input name="stock" type="number"/></label>
-            <button></button>
+        <form onSubmit={handelSubmit}>
+            <label>ID: <input name="id" type="number" value={producto.id}/></label>
+            <label>Descripcion: <input name="descripcion" type="text" value={producto.descripcion}/></label>
+            <label>Precio: <input name="precioUnitario" type="number" value={producto.precioUnitario}/></label>
+            <label>Descuento %: <input name="descuento" type="number" value={producto.descuento}/></label>
+            <label>Stock: <input name="stock" type="number" value={producto.stock}/></label>
+            <button type="submit">{editarProducto ? 'Actualizar':'Agregar'}</button>
         </form>
     );
     

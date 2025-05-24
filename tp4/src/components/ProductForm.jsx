7c4e16e92369963c78ea5719a47be87b84d9186d
Jesica Salas <jessicasalas.as@gmail.com>
2025-05-24 14:21:57 -0300

    Modificacion del css

--- tp4/src/components/ProductForm.jsx
+++ tp4/src/components/ProductForm.jsx
@@ -44,25 +44,25 @@ const ProductForm = ({editarProducto, onAct, onAgregar})=>{
     };
 
     return(
-        <form onSubmit={handelSubmit}>
+        <form onSubmit={handelSubmit} >
 
             <div className="product-a">
 
-                <label><h3>ID:</h3> <input name="id" type="number" value={producto.id} onChange={handelChange} required/></label>
-                <label> <h3>Descripcion:</h3> <input name="descripcion" type="text" value={producto.descripcion} onChange={handelChange} required  /></label>
+                <label><h3>ID : </h3> <input name="id" type="number" value={producto.id} onChange={handelChange} required/></label>
+                <label> <h3>Descripcion : </h3> <input name="descripcion" type="text" value={producto.descripcion} onChange={handelChange} required  /></label>
 
             </div >
             <br/>
             <div className="product-form">
+
                 <label><h3>Precio:</h3> <input name="precioUnitario" type="number" value={producto.precioUnitario} onChange={handelChange} required/></label>
                 <label><h3>Descuento %:</h3> <input name="descuento" type="number" value={producto.descuento} onChange={handelChange} required/></label>
                 <label><h3>Stock:</h3> <input name="stock" type="number" value={producto.stock} onChange={handelChange} required/></label>
                 <button type="submit">{editarProducto ? 'Actualizar':'Agregar'}</button>
+
             </div>
 
         </form>
-
-
 
 
     );

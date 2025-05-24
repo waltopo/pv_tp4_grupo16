2e1646a0bb6ae7aa53df911eab54121998304787
lucasortiz512 <lucasortiz.lmo@gmail.com>
2025-05-24 12:45:14 -0300

    productlist

--- tp4/src/components/ProductList.jsx
+++ tp4/src/components/ProductList.jsx
@@ -1,16 +1,22 @@
 import ProductItem from "./ProductItem";
 
-const ProductList = ({ productos, onEliminar, onEditar }) => (
-  <div>
-    {productos.map(producto => (
-      <ProductItem
-        key={producto.id}
-        producto={producto}
-        onEliminar={onEliminar}
-        onEditar={onEditar}
-      />
-    ))}
-  </div>
-);
+const ProductList = ({ productos = [], onEliminar, onEditar }) => {
+  if (!Array.isArray(productos) || productos.length === 0) {
+    return <p>No hay productos para mostrar.</p>;
+  }
+
+  return (
+    <div>
+      {productos.map((producto) => (
+        <ProductItem
+          key={producto.id}
+          producto={producto}
+          onEliminar={() => onEliminar(producto.id)}
+          onEditar={() => onEditar(producto)}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default ProductList;
